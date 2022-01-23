6a5e4a9e216d5cc8dd750084c9c6bd1a71096d98
NoP77svk <nop77svk@gmail.com>
2022-01-23 14:18:44 +0100

    * slight reformat

--- rtvs-dl.sh
+++ rtvs-dl.sh
@@ -1,34 +1,47 @@
 #!/usr/bin/env bash
+
 # If link is empty exit
 [ -z "$1" ] && echo "Link is empty" && exit
 echo "Download page: "$1
+
 # Download page and extract playlist
 playlist=$(curl -s $1 | grep -i //www.rtvs.sk/json/archive)
 echo "Playlist:" $playlist
+
 # Playlist to array
 playlist_array=($playlist)
+
 # Extract playlist link
 playlist_link=$(echo 'https:'${playlist_array[3]} | sed 's/\"//g')
 echo "Download playlist: "$playlist_link
+
 # Extract line with link to stream
 stream_tmp="$(curl -s $playlist_link)"
 stream_name=$(echo "${stream_tmp%x}" | grep "src\" :" | grep smil | head -1)
 echo "Stream name:" $stream_name
 stream_title=$(echo "${stream_tmp%x}" | grep "title")
 echo "Stream title: "$stream_title
+
 # Stream link name to array
 stream_name_array=($stream_name)
+
 # Extract link and remove quotes and commas
 stream_link=$(echo ${stream_name_array[2]} | sed 's/[\",]//g')
+
 # Extract title
 stream_title=$(echo "$stream_title" | cut -d ":" -f 2)
+
 # Remove ", from end
 stream_title=$(echo "$stream_title" | sed 's/\",//g')
+
 # Reove quotes
 stream_title=$(echo "$stream_title" | sed 's/\"//g')
+
 # Trim
 stream_title=$(echo "$stream_title" | sed 's/^\s//g')
+
 # Replace space to underscore
 stream_title=$(echo "$stream_title" | sed 's/\s/_/g')
 echo "Download stream link: "$stream_link
+
 ffmpeg -i $stream_link -acodec mp3 -vcodec copy $stream_title.mkv
